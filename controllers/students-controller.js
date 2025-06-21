19229ff6e08853140dcfaef47120c710b569fb46
SP Test <simeonpp@abv.bg>
2025-06-21 22:54:14 +0300

    updated deploy use

--- controllers/students-controller.js
+++ controllers/students-controller.js
@@ -2,7 +2,7 @@ function setup(app, students) {
   app.get('/', function(req, res) {
     let model = {
       title: "MVC Example",
-      msg: "Students Registry 2",
+      msg: "Students Registry",
       students: students
     };
     res.render('home', model);
